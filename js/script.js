3677a7ac5f68bbbde74ec85881cbfd15bcdd2d1c
Simone <tritto.simone@gmail.com>
2023-05-24 19:31:44 +0200

    implementFunction_work

--- js/script.js
+++ js/script.js
@@ -1,9 +1,71 @@
+/*  ********variable********** */
+
 const container = document.querySelector('.container');
 const boxItems = [
     {
-        img : 'img / 1.jpg',
+        img : 'img/1.jpg',
+        nome : '1',
+        prezzo: '10€'
+    },
+        {
+        img : 'img/2.jpg',
+        nome : '2',
+        prezzo: '20€'
+    },
+        {
+        img : 'img/3.jpg',
+        nome : '3',
+        prezzo: '30€'
+    },
+        {
+        img : 'img/4.jpg',
+        nome : '4',
+        prezzo: '40€'
+    },
+        {
+        img : 'img/5.jpg',
+        nome : '5',
+        prezzo: '50€'
+    },
+        {
+        img : 'img/6.jpg',
+        nome : '6',
+        prezzo: '60€'
     }
 ];
+
+/*  ********code********** */
+
+implementCard(boxItems);
+
+
+/*  ********function********** */
+
+function implementCard (box) {
+    let card = '';
+    for (let i = 0; i < box.length; i ++) {
+        card = `
+            <div class="card">
+                <img src="${box[i].img}" alt="">
+                <h2>${box[i].nome}</h2>
+                <h3>${box[i].prezzo}</h3>
+            </div>`;
+        container.innerHTML += card;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+// ****************prova implemento card+*********************
+
 // const immagine = {
 //     img : 'img/1.jpg',
 //     nome : 'ciao',
