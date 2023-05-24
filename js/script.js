1753cf992cd4f176379b1390a689ef6f60df04fa
Simone <tritto.simone@gmail.com>
2023-05-24 18:53:31 +0200

    html_css_work

---
+++ js/script.js
@@ -0,0 +1,18 @@
+const container = document.querySelector('.container');
+const boxItems = [
+    {
+        img : 'img / 1.jpg',
+    }
+];
+// const immagine = {
+//     img : 'img/1.jpg',
+//     nome : 'ciao',
+//     prezzo: '10€'
+// };
+
+// container.innerHTML = `
+//     <div class="card">
+//         <img src="${immagine.img}" alt="">
+//         <h2>${immagine.nome}</h2>
+//         <h3>${immagine.prezzo}</h3>
+//     </div>`;
